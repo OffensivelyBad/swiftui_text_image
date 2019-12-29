2e85d343d168d330c94306e87ee0f930e36e2d73
SRoller <sroller@justfab.com>
2019-12-29 15:09:13 -0500

    Initial Commit

---
+++ TextAndImages/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextAndImages
+//
+//  Created by Shawn Roller on 12/29/19.
+//  Copyright © 2019 offensively-bad. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
